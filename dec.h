a040774d7679b785c81dbf07aa35d066a3250ba9
Zuhayr Syed <zuhxyr.s6@hotmail.com>
2022-12-22 00:57:29 -0500

    init

---
+++ dec.h
@@ -0,0 +1,9 @@
+#ifndef TIC_TAC_TOE_DEC_H
+#define TIC_TAC_TOE_DEC_H
+
+void introduction();
+void player_setup();
+void take_turn();
+void end_game();
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "dec.h"
+using namespace std;
+
+int main() {
+	introduction();
+    player_setup();
+	take_turn();
+	end_game();
+
+    // int n = 0;
+    // cout << "Enter: " << endl;
+    // cin >> n;
+    // cout << "You Entered: " << n << endl;
+	// return 0;
+}
