1fdf0454f05bbbc244f2829454ae5446ba4de00a
Zuhayr Syed <zuhxyr.s6@hotmail.com>
2022-12-22 21:59:58 -0500

    create show grid function

--- dec.h
+++ dec.h
@@ -2,6 +2,7 @@
 #define TIC_TAC_TOE_DEC_H
 
 void introduction();
+void show_grid();
 void player_setup();
 void take_turn();
 void end_game();
--- def.cpp
+++ def.cpp
@@ -3,7 +3,38 @@
 using namespace std;
 
 void introduction() {
-    cout<<"test"<<endl;
+    cout<<"\n";
+    cout<<"   *****************\n";
+    cout<<"      Tic-Tac-Toe\n";
+    cout<<"   *****************\n";
+    cout<<"\n";
+
+    cout<<"Press [Enter] to begin: ";
+	getchar(); // to pause screen  
+	cout<<"\n";
+    cout<<"\n";
+	
+    cout<<"Player 1 --> You: X\n";
+    cout<<"Player 2 --> Computer: O\n";
+    cout<<"\n";
+    
+    cout<<"The grid is shown below:\n\n";
+    
+    show_grid();
+
+}
+
+void show_grid() {
+    cout << "     |     |      \n";
+    cout << "  1  |  2  |  3   \n";
+    cout << "_____|_____|_____ \n";
+    cout << "     |     |      \n";
+    cout << "  4  |  5  |  6   \n";
+    cout << "_____|_____|_____ \n";
+    cout << "     |     |      \n";
+    cout << "  7  |  8  |  9   \n";
+    cout << "     |     |      \n\n";
+    cout<<"\n";
 }
 
 void player_setup() {
